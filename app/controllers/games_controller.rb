3d3235e486174eada39e99c6e9f6d2b5d4532652
Chely Ho <adept_inept@yahoo.com>
2018-07-26 20:45:57 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,7 +4,7 @@ class GamesController < ApplicationController
     end
 
     def create
-      @game = Game.create(post_params)
+      @game = Game.create(game_params)
       render json: @game, status: 201
     end
 
